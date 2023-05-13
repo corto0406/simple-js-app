b4dbda3a05bb081d738d5b45bd5c347283789bf5
Nemanja CF <nemanjacf@Biljanas-iMac.lan>
2023-05-13 21:47:59 +0200

    immersion 1.1 task

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+alert('Hello world');
+
+let name='favoriteFood';
+document.write(name);
+let food1='pizza';
+document.write(food1);
+let food2 ='hamburger';
+document.write(food2);
+let food3='pasta';
+document.write(food3);
+
+
