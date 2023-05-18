316c3e65d4d4680585392e4c56ceb084ac580a92
Nemanja CF <nemanjacf@Biljanas-iMac.lan>
2023-05-18 14:39:34 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-let name='pokemonList';
-let pikachu={
-    name:'Pikachu',
-    heigh:3,
-    color:'yellow'
-};
-let snorlax={
-    name:'Snorlax',
-    heigh:9,
-    color:'blue'
-};
-let charizard={
-    name:'Charizard',
-    heigh:12,
-    color:'orange'
-};
+let pokemonList = [
+    {
+        name: 'Pikachu',
+        heigh: 3,
+        color: 'yellow'
+    },
+    {
+        name: 'Snorlax',
+        heigh: 9,
+        color: 'blue'
+    },
+    {
+        name: 'Charizard',
+        heigh: 12,
+        color: 'orange'
+    }
+
+];
