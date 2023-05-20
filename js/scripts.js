b72dcc55099847c8be097da810a29c9ff31b2a79
Nemanja CF <nemanjacf@Biljanas-iMac.lan>
2023-05-20 21:40:29 +0200

    immersion 1.3.
    write some loops for our new project

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,29 @@ let pokemonList = [
     }
 
 ];
+//checking height of each pokemon
+//also include solutions for third part of task
+for (let i=0; i< pokemonList.length; i++) {
+    if (pokemonList[i].height <11 && pokemonList[i].height >5){
+        console.log(pokemonList[i].name + " (height:9)");
+    }else if (pokemonList[i].height<5){
+        console.log(pokemonList[i].name + " (height:3)");
+    }else {
+        console.log(pokemonList[i].name + " (height:12)-Wow,that's big!")
+    }
+    };
+//check why this loop don't work with tutor! 
+for (let i=0; i<= pokemonList.color; i++) {
+    if (pokemonList[i].color === orange){
+        console.log(pokemonList.name + " has pretty look");
+    }if (pokemonList[i].color === blue){
+        console.log(pokemonList.name);
+    }if (pokemonList[i].color === yellow){
+        console.log(pokemonList.name);
+    }
+};
+
+let name=['Pikachu','Snorlax','Charizard'];
+document.write (name); 
+
+
