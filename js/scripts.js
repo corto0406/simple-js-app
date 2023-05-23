bb63c91b80fd828c9758823f5506ebb1c79bcb90
Nemanja CF <nemanjacf@Biljanas-iMac.lan>
2023-05-23 20:31:53 +0200

    immersion 1.3. redo
    Do some changes

--- js/scripts.js
+++ js/scripts.js
@@ -18,27 +18,27 @@ let pokemonList = [
 ];
 //checking height of each pokemon
 //also include solutions for third part of task
-for (let i=0; i< pokemonList.length; i++) {
-    if (pokemonList[i].height <11 && pokemonList[i].height >5){
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 11 && pokemonList[i].height > 5) {
         console.log(pokemonList[i].name + " (height:9)");
-    }else if (pokemonList[i].height<5){
+    } else if (pokemonList[i].height < 5) {
         console.log(pokemonList[i].name + " (height:3)");
-    }else {
+    } else {
         console.log(pokemonList[i].name + " (height:12)-Wow,that's big!")
     }
-    };
+};
 //check why this loop don't work with tutor! 
-for (let i=0; i<= pokemonList.color; i++) {
-    if (pokemonList[i].color === orange){
-        console.log(pokemonList.name + " has pretty look");
-    }if (pokemonList[i].color === blue){
-        console.log(pokemonList.name);
-    }if (pokemonList[i].color === yellow){
-        console.log(pokemonList.name);
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].color === "orange") {
+        console.log(pokemonList[i].name + " has pretty look");
+    } if (pokemonList[i].color === "blue") {
+        console.log(pokemonList[i].name);
+    } if (pokemonList[i].color === "yellow") {
+        console.log(pokemonList[i].name);
     }
 };
 
-let name=['Pikachu','Snorlax','Charizard'];
-document.write (name); 
-
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write ("<p class=pokemon>" + pokemonList[i].name + '-' + pokemonList[i].height + "<br>" + "</p>")
+}
 
